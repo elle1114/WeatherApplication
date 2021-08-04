aaf1c89a81407bf686bc98d16eebe29dc7757a9a
paulopez <84214454+elle1114@users.noreply.github.com>
2021-08-04 11:01:37 +0800

    changes

---
+++ weather-app-ferry/src/app/weather-forecast/weather-forecast.component.ts
@@ -0,0 +1,43 @@
+import { Component, OnInit } from '@angular/core';
+import { ForecastService } from '../forecast.service'
+
+@Component({
+  selector: 'app-weather-forecast',
+  templateUrl: './weather-forecast.component.html',
+  styleUrls: ['./weather-forecast.component.css']
+})
+export class WeatherForecastComponent implements OnInit {
+  lat:any;
+  lon: any;
+
+  loc:any;
+  for:any;
+  constructor(private forecastService: ForecastService) { }
+
+  ngOnInit(): void {
+    navigator.geolocation.getCurrentPosition(position => {
+      this.lat = position.coords.latitude;
+      this.lon = position.coords.longitude;
+
+      this.forecastService.getCurrentLoc(this.lon,this.lat).subscribe(data => {
+        this.loc=data;
+        console.log('Call MapBox API: ' , this.loc)
+      })
+      this.forecastService.getWeatherForecast(this.lat, this.lon).subscribe(data => {
+        this.for=data;
+        console.log('Call Weather Forecast API: ' , this.for)
+      })
+    })
+  }
+  onSearchClick(data:any){
+    this.forecastService.getPlaces(data).subscribe((res) => {
+      this.loc=res;
+      console.log('New Location: ' , this.loc.features[0].text)
+    })
+    this.forecastService.getWeatherForecast(this.loc.features[0].center[1],this.loc.features[0].center[0]).subscribe(data => {
+      this.for=data;
+      console.log('New Forecast: ' , this.for )
+    })
+  }
+
+}
